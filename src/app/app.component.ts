f89e3a439879cfbe8f4f4ce7da4734e411f5c05e
Moeen Ahmad <moeahmad@etisalat.ae>
2022-09-08 13:07:43 +0400

    Custom JSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,37 +7,79 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'carousel';
-  let slideIndex = 1;
-  showSlides(slideIndex);
-  
-  // Next/previous controls
-  
-  plusSlides = (n) => {
-    return showSlides(slideIndex += n);
+  itemToShow:any = 1
+
+  ngOnInit() {
+
+   
+
+   
+
   }
+
+  next(){
+
+    if(this.itemToShow == 3){
+
+        this.itemToShow = 1
+
+    }
+
+    else{
+
+        this.itemToShow+=1
+
+    }
+
+  }
+
+  previous(){
+
+    if(this.itemToShow == 1){
+
+        this.itemToShow = 3
+
+    }
+
+    else{
+
+        this.itemToShow-=1
+
+    }
+
+  }
+
+  // slideIndex : any = 1;
+  // this.showSlides(this.slideIndex);
   
-  // Thumbnail image controls
+  // // Next/previous controls
   
-  currentSlide = (n) => {
-    showSlides(slideIndex = n);
-  } 
+  // plusSlides (n : any)  {
+  //   return this.showSlides(this.slideIndex += n);
+  // }
+  
+  // // Thumbnail image controls
+  
+  // currentSlide (n: any)  {
+  //   this.showSlides(slideIndex = n);
+  // } 
   
 
-  showSlides = (n) => {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1} 
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block"; 
-    dots[slideIndex-1].className += " active"; 
-  }
+  // showSlides (n:any) {
+  //   let i;
+  //   let slides = document.getElementsByClassName("mySlides");
+  //   let dots = document.getElementsByClassName("dot");
+  //   if (n > slides.length) {slideIndex = 1} 
+  //   if (n < 1) {slideIndex = slides.length}
+  //   for (i = 0; i < slides.length; i++) {
+  //     slides[i].style.display = "none"; 
+  //   }
+  //   for (i = 0; i < dots.length; i++) {
+  //     dots[i].className = dots[i].className.replace(" active", "");
+  //   }
+  //   slides[slideIndex-1].style.display = "block"; 
+  //   dots[slideIndex-1].className += " active"; 
+  // }
 
   
   
